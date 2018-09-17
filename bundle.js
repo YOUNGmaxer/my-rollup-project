103ad2a5df0df1d3f80bfaa30541ef7dd030636e
v_zminyang(杨泽民) <v_zminyang@tencent.com>
2018-09-17 18:03:20 +0800

    first commit

---
+++ bundle.js
@@ -0,0 +1,15 @@
+'use strict';
+
+require('lodash');
+
+var index = 42;
+
+// import foo from './foo.js';
+
+function main$1 () {
+  // console.log(foo);
+  // console.log('version: ' + version);
+  console.log('the answer is ' + index);
+}
+
+module.exports = main$1;
